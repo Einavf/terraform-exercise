f02aaa7fb7debf1f51e48a9661601e5e970b3e4a
einavf <einav.friedman@kenshoo.com>
2017-09-11 13:24:21 +0300

    adding security group and port

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-40d28157"
+  ami           = "ami-10547475"
   instance_type = "t2.micro"
 
   user_data = <<-EOF
@@ -15,4 +15,14 @@ resource "aws_instance" "example" {
   tags {
        Name = "terraform-example"
        }
+
+  resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+  ingress {
+           from_port   = 8080
+           to_port     = 8080
+           protocol    = "tcp"
+           cidr_blocks = ["0.0.0.0/0"]
+         }
+  }
 }
